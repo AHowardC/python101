00e08dcb16eb89c5d5fc799e652f4850103e22e1
AHowardC <achsvgusaeng@yahoo.com>
2017-09-07 10:39:20 -0400

    py stuff

---
+++ newPythonFile.py
@@ -0,0 +1,11 @@
+print "Hello, World"
+print("Hello, World")
+name = "akil" + "kjlkj"
+print "Hello {} {}".format(first_name,last_name)
+print "Hello %s" % "akil"
+
+# Booleans
+the_truth = True;
+print type(the_truth)
+
+name = raw_input("whats your name")
---
+++ python101.py
@@ -0,0 +1,20 @@
+# if 1 == 2: 
+# 	print "True"
+# else:
+# 	print "F"
+
+# # raw_input is always collected as a string
+
+# import random; 
+
+		secret_number = 5
+		get_answer = raw_input("please hit any key")
+
+		if get_answer == secret_number:
+			print "you win"
+		else:
+			print "try again"
+
+
+
+
